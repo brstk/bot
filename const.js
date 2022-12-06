2ab98f72fee8b60af969fda46e813dfcdc3953da
Tanya <9152204088zz@gmail.com>
2022-12-06 18:40:16 +0300

    release 1

---
+++ const.js
@@ -0,0 +1,7 @@
+const commands = `
+/start - Перезапустить бота
+/help - Помощь
+`;
+
+// в объекте exports создаем свойство commands, которому присваиваем нашу переменную commands
+module.exports.commands = commands;
